acefa27cc5cf56b5e50d0370ebf4939ef88ebbc3
seshanb <125367985+seshanb@users.noreply.github.com>
2023-12-26 19:41:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
-
-  acl    = "private"  # Access control for the bucket, you can modify as needed
-
-  versioning {
-    enabled = true  # Enable versioning for the bucket
-  }
-
+ 
   tags = {
     Name        = "MyS3Bucket"
     Environment = "Production"
