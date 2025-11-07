452c499d4e5b2d7ec83571ea205537c236cf960c
seshanb <125367985+seshanb@users.noreply.github.com>
2025-11-07 11:01:49 +0530

    Replace AWS instance and data source with S3 bucket

--- main.tf
+++ main.tf
@@ -1,24 +1,8 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
 
   tags = {
-    Name = var.instance_name
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
